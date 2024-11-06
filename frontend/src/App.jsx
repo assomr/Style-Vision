850123b46a4b748401a931a7c84ab25b1d9ab51e
Maissoum <maissem.ooumri@gmail.com>
2024-11-06 18:26:15 +0100

    Add routes for pages: About, Contact, Home, Login, MyAppointments, MyProfile, Stylists

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import {Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import Stylists from './pages/Stylists'
+import Login from './pages/Login'
+import About from './pages/About'
+import Contact from './pages/Contact'
+import MyProfile from './pages/MyProfile'
+import MyAppointments from './pages/MyAppointments'
+import Appointment from './pages/Appointment'
+
 
 const App = () => {
   return (
     <div className='mx-4 sm:mx-[10%]'>
        
        <Routes>
-        <Route path='/' eleùent={<Home />} />
+        <Route path='/' element={<Home />} />
+        <Route path='/stylists' element={<Stylists />} />
+        <Route path='/stylists/:speciality' element={<Stylists />} />
+        <Route path='/login' element={<Login />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+        <Route path='/my-profile' element={<MyProfile />} />
+        <Route path='/my-appointments' element={<MyAppointments />} />
+        <Route path='/appointment/:stylId' element={<Appointment />} />
        </Routes>
     </div>
   )
